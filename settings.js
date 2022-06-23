e7fbaab64afb180c313fea93368f2709903a3af5
zodress <108037881+zodress@users.noreply.github.com>
2022-06-23 19:25:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281528656244','6281528656244','6281528656244']
-global.premium = ['6281528656244']
-global.ownernomer = '6281528656244'
-global.ownername = 'Tn.Marshal'
-global.botname = 'McQueenBots-Md'
-global.footer = 'Tn.Marshal'
+global.owner = ['6288973218166','6288973218166','6288973218166']
+global.premium = ['6288973218166']
+global.ownernomer = '6288973218166'
+global.ownername = 'ZO.Dress'
+global.botname = 'KurumiBots-Md'
+global.footer = 'ZO.Dress'
 global.ig = 'https://instagram.com/reihan_arthur?igshid=YmMyMTA2M2Y='
-global.email = 'Marshal@gmail.com'
+global.email = 'Zodress@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://bit.ly/3Om8D5j'
-global.packname = 'McQueenBots-Md'
-global.author = 'omaga'
+global.packname = 'KurumiBots-Md'
+global.author = 'Ganteng'
 global.sessionName = 'chika'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
