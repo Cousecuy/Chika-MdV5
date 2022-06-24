f914cd67abc5d1910f0f9bad8a612ab34462f4d5
zodress <108037881+zodress@users.noreply.github.com>
2022-06-24 22:27:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,18 +18,18 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281528656244','6281528656244','6281528656244']
-global.premium = ['6281528656244']
-global.ownernomer = '6281528656244'
-global.ownername = 'Tn.McMissile'
-global.botname = 'McQueen-Bots'
-global.footer ='Tn.McMissile'
+global.owner = ['6281938642441','6281938642441','6281938642441']
+global.premium = ['6281938642441']
+global.ownernomer = '6281938642441'
+global.ownername = 'Bilxx Semtur'
+global.botname = 'DIO-Bots'
+global.footer ='Bilxx Semtur'
 global.ig = 'https://instagram.com/reihan_arthur?igshid=YmMyMTA2M2Y='
-global.email = 'McMissile@gmail.com'
+global.email = 'Bilxxsentur@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://bit.ly/3Om8D5j'
-global.packname = 'McMissile'
+global.packname = 'Bilxx semtur'
 global.author = 'Ganteng'
 global.sessionName = 'chika'
 global.prefa = ['','!','.','🐦','🐤','🗿']
