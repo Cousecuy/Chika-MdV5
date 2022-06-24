a4b3ab1dbbc6685712696637b7f5d6c8a402f0b1
zodress <108037881+zodress@users.noreply.github.com>
2022-06-24 11:11:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,18 +18,18 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6281291482615','6281291482615','6281291482615']
-global.premium = ['6281291482615']
-global.ownernomer = '6281291482615'
-global.ownername = 'CLayy'
-global.botname = 'Farsbots-Md'
-global.footer ='CLayy'
+global.owner = ['6281528656244','6281528656244','6281528656244']
+global.premium = ['6281528656244']
+global.ownernomer = '6281528656244'
+global.ownername = 'Tn.McMissile'
+global.botname = 'McQueenBots-Md'
+global.footer ='Tn.McMissile'
 global.ig = 'https://instagram.com/reihan_arthur?igshid=YmMyMTA2M2Y='
-global.email = 'CLayy@gmail.com'
+global.email = 'McMissile@gmail.com'
 global.region = 'Indonesia'
 global.sc = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.myweb = 'https://bit.ly/3Om8D5j'
-global.packname = 'Farsbot-Md'
+global.packname = 'McQueenBots-Md'
 global.author = 'Ganteng'
 global.sessionName = 'chika'
 global.prefa = ['','!','.','🐦','🐤','🗿']
